14f5e20c48695fc77b7931d69a1e29dc888c78de
Liuba <kroka.luba@gmail.com>
2017-07-14 14:32:29 +0300

    YO:
    1) change questions

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,33 +4,33 @@ const chalk = require('chalk');
 const yosay = require('yosay');
 
 module.exports = class extends Generator {
-  prompting() {
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the sublime ' + chalk.red('generator-first') + ' generator!'
-    ));
+    prompting() {
+        return this.prompt(
+                [{
+                        type: 'input',
+                        name: 'name',
+                        message: 'Your project name',
+                        default: this.appname // Default to current folder name
+                    }, {
+                        type: 'confirm',
+                        name: 'cool',
+                        message: 'Would you like to enable the Cool feature?'
+                    }]
+                )
+                .then((answers) => {
+                    this.log('app name', answers.name);
+                    this.log('cool feature', answers.cool);
+                });
+    }
 
-    const prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+    writing() {
+        this.fs.copy(
+                this.templatePath('dummyfile.txt'),
+                this.destinationPath('dummyfile.txt')
+                );
+    }
 
-    return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
-    });
-  }
-
-  writing() {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
-  }
-
-  install() {
-    this.installDependencies();
-  }
+    install() {
+        this.installDependencies();
+    }
 };
